5a1e4b71c0c72fe4878eecc60faca14d4a763c75
Philip Keller <philipjoelkeller@gmail.com>
2023-03-15 11:46:41 -0700

    Initial Commit

---
+++ SwiftfulBootcamp/SwiftfulBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulBootcampApp.swift
+//  SwiftfulBootcamp
+//
+//  Created by Philip Keller on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
